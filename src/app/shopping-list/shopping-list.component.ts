162f36f4dbb341792b02451dbf8d727881533eea
patwarisuraj12 <patwarisuraj12@gmail.com>
2022-09-02 23:56:12 +0530

    services used for shopping list appication wide

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,20 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 
+import { ShoppingListService } from './shopping.service';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+  ingredients: Ingredient[];
 
-  constructor() { }
+  constructor( private shoppingListService : ShoppingListService ) { }
 
   ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredient();
   }
 
   onIngAdded(ingAdded : Ingredient){
